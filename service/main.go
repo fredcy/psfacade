1258a64f400e6dce69d166b62b577866c424c8af
Fred Yankowski <fcy@imsa.edu>
2016-10-12 09:40:02 -0500

    Use gorilla and scheme for allow-origin

--- service/main.go
+++ service/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/fredcy/psfacade"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
@@ -56,12 +57,38 @@ var address = flag.String("address", ":8080", "Listen and serve at this address"
 
 func main() {
 	flag.Parse()
+
 	dsn := os.Getenv("PS_DSN")
 	db, err := sql.Open("oci8", dsn)
 	if err != nil {
 		log.Panic(err)
 	}
-	http.HandleFunc("/students", wraptimer(wrapdb(studentshandler, db)))
+
+	r := mux.NewRouter()
+	r.HandleFunc("/students", wraptimer(wrapdb(studentshandler, db)))
+	http.Handle("/", &MyServer{r})
+
 	log.Printf("Listening at %s", *address)
 	log.Fatal(http.ListenAndServe(*address, nil))
+}
+
+/* See http://stackoverflow.com/questions/12830095/setting-http-headers-in-golang about CORS */
+
+type MyServer struct {
+	r *mux.Router
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+	// Lets Gorilla work
+	s.r.ServeHTTP(rw, req)
 }
